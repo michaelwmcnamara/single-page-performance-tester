9105c154725a46c18ead745cfe2afab18e685d7c
Michael McNamara <michael.mcnamara@theguardian.com>
2017-05-04 12:58:51 +0100

    more tidying

--- src/main/scala/app/api-utils/ArticleUrls.scala
+++ src/main/scala/app/api-utils/ArticleUrls.scala
@@ -19,7 +19,7 @@ class ArticleUrls(key: String) {
   val pageSize: Int = 30
 // todo get top 20 for each section
 // You can do this per section, for example for the uk section:
-// http://someExampleUrl/uk?show-most-viewed=true&page-size=0
+// http://some.example.url/uk?show-most-viewed=true&page-size=0
 
   def getUrlsForContentType(contentType: String): List[(Option[ContentFields], Seq[Tag], String)] = {
      contentType match {
